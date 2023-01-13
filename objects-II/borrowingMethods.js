461a9c314e00f371103f5e8af415a1778a893085
blakerunyon09 <109623833+blakerunyon-paper@users.noreply.github.com>
2023-01-12 17:26:07 -0700

    Notes

--- objects-II/borrowingMethods.js
+++ objects-II/borrowingMethods.js
@@ -2,5 +2,6 @@ const { assert } = require("../test/assert.js");
 
 let map = {one: true, two: true, hasOwnProperty: true};
 
+// I don't 100% follow this solution. Used the book.
 assert("It writes over a known method", Object.prototype.hasOwnProperty.call(map, "one"))
   .toBeTruthy();
